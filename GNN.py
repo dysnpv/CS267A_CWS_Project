1e08e57a31288faba8391fde3a6af56a68fccdca
Albert <47127602+dysnpv@users.noreply.github.com>
2022-06-10 23:36:56 -0700

    Add files via upload

--- GNN.py
+++ GNN.py
@@ -93,7 +93,68 @@ class GCN(torch.nn.Module):
             y = torch.LongTensor(labels[:(len(labels) - 1)]).to('cuda')
             loss = loss_fct(result, y)
         return result, loss
-    
+
+class Naive_GCN_lexicon(torch.nn.Module):
+    def __init__(self):
+        super(Naive_GCN_lexicon, self).__init__()
+        self.conv1 = GCNConv(768, 768).to('cuda')
+        self.conv2 = GCNConv(768, 768).to('cuda')
+        self.conv3 = GCNConv(768, 768).to('cuda')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case = False)
+        self.model = BertModel.from_pretrained('bert-base-multilingual-cased', do_lower_case = False, output_hidden_states=True).to('cuda')
+        self.classifier = DropoutClassifier(768 * 2, 2).to('cuda')
+        
+    def forward(self, input_tokens, labels = None):
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(input_tokens)
+        tokens_tensor = torch.tensor([indexed_tokens]).to('cuda')
+        outputs = self.model(tokens_tensor)
+        hidden_states = outputs[2]
+        assert(len(input_tokens) == hidden_states[0].shape[1])
+        
+        sentence = ''.join(input_tokens)
+        
+        l_cut = jieba.lcut(sentence, cut_all = True)
+        l_cut_np = []
+        [l_cut_np.append(x) for x in l_cut if x not in l_cut_np]
+        
+        edge_index_list = []
+        for w in l_cut_np:
+            if len(w) < 2:
+                continue
+            index = sentence.find(w)
+            while index != -1:
+                edge_index_list.append([index, index + len(w) - 1])
+                edge_index_list.append([index + len(w) - 1, index])
+                index = sentence.find(w, index + 1)
+            
+        edge_index = torch.LongTensor(edge_index_list).t().contiguous().to('cuda')
+        
+        x = hidden_states[0][0].to('cuda')
+        
+        # If there are no edges, then there's no need to do GCN.
+        if edge_index.shape[0] == 0:
+            h = x
+        else:
+            h = self.conv1(x, edge_index)
+            h = h.tanh()
+            h = self.conv2(h, edge_index)
+            h = h.tanh()
+            h = self.conv3(h, edge_index)
+            h = h.tanh()
+            
+        assert(len(h) == len(input_tokens))
+        processed_list = []
+        for i in range(len(h) - 1):
+            processed_list.append(torch.unsqueeze(torch.cat((h[i], h[i + 1]), 0), 0))
+        processed_tensor = torch.cat(processed_list, 0).to('cuda')
+        result = self.classifier(processed_tensor)
+        loss = None
+        if labels is not None:
+            loss_fct = torch.nn.CrossEntropyLoss()
+            y = torch.LongTensor(labels[:(len(labels) - 1)]).to('cuda')
+            loss = loss_fct(result, y)
+        return result, loss
+
 class GCN_lexicon(torch.nn.Module):
     def __init__(self):
         super(GCN_lexicon, self).__init__()
--- training.py
+++ training.py
@@ -1,8 +1,7 @@
-import jieba
 import torch
 import random
 from BERT_Model import BertForWordSegmentation, Naive_BertForWordSegmentation
-from GNN import Naive_GCN, GCN, GCN_lexicon
+from GNN import Naive_GCN, GCN, Naive_GCN_lexicon, GCN_lexicon
 from ReadData import is_english, read_from_training_data, read_from_testing_data
     
 def data_loader(x_list, y_list):
@@ -44,10 +43,10 @@ def train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, 
     
     partition = int(len(x_list) * 4 / 5)
 
-    best_model = model
     best_acc = 0.0
     
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    model.train()
     for epoch in range(num_epochs):
         train_loader = data_loader(x_list[:partition], y_list[:partition])
         test_loader = data_loader(x_list[partition:], y_list[partition:])
@@ -74,11 +73,9 @@ def train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, 
         print('Test Accuracy: %.4f' % (correct_predictions * 1.0 / num_characters))
         if correct_predictions * 1.0 / num_characters > best_acc:
             best_acc = correct_predictions * 1.0 / num_characters
-            best_model = model
-        model.train()
+            if do_save:
+                torch.save(model, save_path)
         torch.cuda.empty_cache()
-    if do_save:
-        torch.save(best_model, save_path)
 
 def test_model(model_path, filename, num_sentences):
     model = torch.load(model_path)
@@ -97,71 +94,52 @@ def test_model(model_path, filename, num_sentences):
             num_characters += 1
     print('Test Accuracy: %.4f' % (correct_predictions * 1.0 / num_characters))
     
-def prepare_script(model_path, test_file, output_filename = 'FineTune_result.txt'):
-    model = torch.load(model_path)
-    
-    test_chars = open(test_file).read()
-    characters = []
-    hash_string = ""
-    for c in test_chars:
-        if is_english(c):
-            hash_string += c
-        else:
-            if hash_string != "":
-                characters.append(hash_string)
-                hash_string = ""        
-            characters.append(c)
-            
-    x_list = read_from_testing_data(test_file)
-    output = open(output_filename, "w+")
-
-    character_id = 0
-    for x in x_list:
-        if len(x) == 0:
-            print("Error! Sentecen with length 0!")
-            continue
-        if len(x) > 1:
-            z, _ = model(x)
-    #        print(z)
-    #        print(z.argmax())
-            for (i, prediction) in enumerate(z):
-                output.write(characters[character_id])
-                if prediction.argmax().item() == 1:
-                    output.write("  ")
-                character_id += 1
-        output.write(characters[character_id])
-        character_id += 1
-#       print("space: %d %c" % (character_id, characters[character_id]))
-        output.write("  ")
-        if characters[character_id] == '\n':
-#                print("end of line: %d %d %d" % (sentence_id, token_id, character_id)).
-            output.write("\n")
-            character_id += 1
-    torch.cuda.empty_cache()
-    output.close()
         
-def train_Naive_BERT(train_file, num_sentences, num_epochs = 15, learning_rate = 0.005, do_save = True, save_path = 'Naive_BERT.bin', eliminate_one = True):
+def train_Naive_BERT(train_file, num_sentences, num_epochs = 20, learning_rate = 0.005, do_save = True, save_path = 'Naive_BERT.bin', eliminate_one = True):
     model = Naive_BertForWordSegmentation()
     train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, save_path, eliminate_one)
     torch.cuda.empty_cache()
 
         
-def train_BERT(train_file, num_sentences, num_epochs = 15, learning_rate = 0.005, do_save = True, save_path = 'BERT.bin', eliminate_one = True):
+def train_BERT(train_file, num_sentences, num_epochs = 20, learning_rate = 0.005, do_save = True, save_path = 'BERT.bin', eliminate_one = True):
     model = BertForWordSegmentation()
     train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, save_path, eliminate_one)
     torch.cuda.empty_cache()
 
-def train_Naive_GCN(train_file, num_sentences, num_epochs = 15, learning_rate = 0.005, do_save = True, save_path = 'Naive_GCN.bin', eliminate_one = True):
+def train_Naive_GCN(train_file, num_sentences, num_epochs = 30, learning_rate = 0.001, do_save = True, save_path = 'Naive_GCN.bin', eliminate_one = True):
     model = Naive_GCN()
     train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, save_path, eliminate_one)
     torch.cuda.empty_cache()
 
-def train_GCN(train_file, num_sentences, num_epochs = 15, learning_rate = 0.005, do_save = True, save_path = 'GCN.bin', eliminate_one = True):
+def train_GCN(train_file, num_sentences, num_epochs = 30, learning_rate = 0.001, do_save = True, save_path = 'GCN.bin', eliminate_one = True):
     model = GCN()
     train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, save_path, eliminate_one)
     torch.cuda.empty_cache()
 
-def train_GCN_lexicon(train_file, num_sentences, num_epochs = 15, learning_rate = 0.005, do_save = True, save_path = 'GCN_lexicon.bin', eliminate_one = True):
+def train_Naive_GCN_lexicon(train_file, num_sentences, num_epochs = 30, learning_rate = 0.001, do_save = True, save_path = 'Naive_GCN_lexicon.bin', eliminate_one = True):
+    model = Naive_GCN_lexicon()
+    train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, save_path, eliminate_one)
+    torch.cuda.empty_cache()
+
+def train_GCN_lexicon(train_file, num_sentences, num_epochs = 30, learning_rate = 0.001, do_save = True, save_path = 'GCN_lexicon.bin', eliminate_one = True):
     model = GCN_lexicon()
     train(train_file, num_sentences, model, num_epochs, learning_rate, do_save, save_path, eliminate_one)
     torch.cuda.empty_cache()
+    
+if __name__ == "__main__":
+    torch.cuda.empty_cache()
+    
+    print("GCN_lexicon:")
+    train_GCN_lexicon("pku_training.txt", 2000, 30, learning_rate = 0.001)
+    test_model("GCN_lexicon.bin", "pku_test_gold.txt", 5000)
+    print("\n")
+    
+    print("GCN:")
+    train_GCN("pku_training.txt", 2000, 30, learning_rate = 0.001)
+    test_model("GCN.bin", "pku_test_gold.txt", 5000)
+    print("\n")
+    
+    print("Naive_GCN:")
+    train_Naive_GCN("pku_training.txt", 2000, 30, learning_rate = 0.001)
+    test_model("Naive_GCN.bin", "pku_test_gold.txt", 5000)
+    print("\n")
